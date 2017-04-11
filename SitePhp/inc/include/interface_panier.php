f1aac0544de7336f62a16a147e19e193b9db5cbf
Romtaro <montgrandiromain@gmail.com>
2017-04-11 15:50:48 +0200

    Project v0.0.2

--- SitePhp/inc/include/interface_panier.php
+++ SitePhp/inc/include/interface_panier.php
@@ -2,7 +2,7 @@
 Interface public Unpannier{
 $titre;
 $id_produit;
-$quantite
+$quantite;
 function public ajout_panier();
 function public vider_panier();
 function public Montant_total();
---
+++ SitePhp/inc/include/interface_produit.php
@@ -0,0 +1,12 @@
+<?php
+Interface public Produit {
+$id_produit;
+$titre;
+$categorie;
+$couleur;
+$taille;
+$photo;
+$description;
+$prix;
+$quantite;
+ ?>
