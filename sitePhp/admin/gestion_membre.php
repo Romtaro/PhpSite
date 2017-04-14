e02fc34445eefe925a717c1e18bc07e432c877a5
Romtaro <montgrandiromain@gmail.com>
2017-04-14 02:50:13 +0200

    MEMBREADMINGESTION OKKKKKKKKKK

--- sitePhp/admin/gestion_membre.php
+++ sitePhp/admin/gestion_membre.php
@@ -15,9 +15,10 @@ if(isset($_GET['msg']) && $_GET['msg'] == "supok")
 require_once("../inc/haut.inc.php");
 //require_once("../inc/menu.inc.php");
 echo '<h1> Voici les membres inscrit au site </h1>';
-	$resultat = Database::query("SELECT * FROM membre");
-	echo "Nombre de membre(s) : " . count($resultat);
+	$resultats = Database::queryq("SELECT * FROM membre");
+	echo "Nombre de membre(s) : " . count($resultats);
 	echo "<table style='border-color:red' border=10> <tr>";
+		$resultat = Database::queryp("SELECT * FROM membre");
 	foreach($resultat as $key => $colonne)
 	//debug($colonne);
 	{
@@ -25,14 +26,17 @@ echo '<h1> Voici les membres inscrit au site </h1>';
 	}
 	echo '<th> Supprimer </th>';
 	echo "</tr>";
-	foreach($resultat as $key => $membre)
+	foreach($resultats as $key => $membre)
 	{
+	  //debug($membre);
 		echo '<tr>';
 		foreach ($membre as $key => $information)
 		{
+
+		//	debug($information);
 			echo '<td>' . $information . '</td>';
 		}
 		echo "<td><a href='gestion_membre.php?msg=supok&&id_membre=" . $membre['id_membre'] . "' onclick='return(confirm(\"Etes-vous sûr de vouloir supprimer ce membre?\"));'> X </a></td>";
 		echo '</tr>';
-	}
+}
 	echo '</table>';
