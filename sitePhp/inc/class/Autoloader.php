baf5cfda9a88bd635a3dd6d16c92c27201abb54b
Romtaro <montgrandiromain@gmail.com>
2017-04-14 16:28:20 +0200

    autolo

--- sitePhp/inc/class/Autoloader.php
+++ sitePhp/inc/class/Autoloader.php
@@ -2,14 +2,19 @@
 
 class Autoloader{
   static function autoload($class){
-
+if($class =! ""){
     require_once"$class.php";
+  }else {
+    require_once "inc/$class.php";
+  }
 
 
   }
     static function register(){
       spl_autoload_register(array('Autoloader','autoload'));
     }
+
+
 
 }
 ?>
